84671268763d8e692a8481584ad486465186bb3b
kota <moon.rewrite@gmail.com>
2021-06-20 23:11:59 +0900

    LEDを１つ点灯させる

---
+++ LED_right/Single_LED.py
@@ -0,0 +1,20 @@
+'''
+LEDを1つ光らせる
+'''
+
+import wiringpi as pi
+import time
+
+# 赤線でRaspberry Piから出ている箇所のGPIO番号
+LED_PIN = 23
+
+pi.wiringPiSetupGpio()
+# GPIOを出力モードに設定
+pi.pinMode(LED_PIN, pi.OUTPUT)
+
+while True:
+    pi.digitalWrite(LED_PIN, pi.LOW)
+    time.sleep(1)
+
+    pi.digitalWrite(LED_PIN, pi.HIGH)
+    time.sleep(1)
